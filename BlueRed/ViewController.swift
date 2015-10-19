46d4b49d68212c29f5b52c9922d28d9c9f5842bf
javen123 <thisjaven@gmail.com>
2015-10-19 16:27:51 -0500

    First commit

---
+++ BlueRed/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  BlueRed
+//
+//  Created by Jim Aven on 10/19/15.
+//  Copyright © 2015 Jim Aven. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var donger: UIImageView!
+    @IBOutlet weak var moose: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func btnHideDongerPressed(sender: AnyObject) {
+        
+        if (donger.hidden == true){
+            donger.hidden = false
+        }
+        else {
+            donger.hidden = true
+        }
+        
+    }
+    @IBAction func btnHideMoosePressed(sender: AnyObject) {
+        
+        if (moose.hidden == true){
+            moose.hidden = false
+        }
+        else {
+            moose.hidden = true
+        }
+
+    }
+}
+
